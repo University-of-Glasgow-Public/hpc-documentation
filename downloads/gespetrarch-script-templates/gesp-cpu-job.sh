fc3a7e039e63ef42da46b21c952bc8720344f5d7
Passant Hafez <passant.hafez@glasgow.ac.uk>
2026-07-02 15:11:17 +0100

    adding copies for gesp templates

---
+++ downloads/gespetrarch-script-templates/gesp-cpu-job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+############# SLURM SETTINGS #############
+#SBATCH --job-name=myjob        # some descriptive job name of your choice
+#SBATCH --partition=cpu         # which partition to use, default "cpu"
+#SBATCH --time=0-01:00:00       # time limit for the whole run, in the form of d-hh:mm:ss
+#SBATCH --mem=1G                # memory required per node, in the form of [num][M|G|T]
+#SBATCH --nodes=1               # number of nodes to allocate, default is 1
+#SBATCH --ntasks=1              # number of Slurm tasks to be launched, increase for multi-process runs ex. MPI
+#SBATCH --cpus-per-task=1       # number of processor cores to be assigned for each task, default is 1, increase for multi-threaded runs
+#SBATCH --ntasks-per-node=1     # number of tasks to be launched on each allocated node
+
+############# SOFTWARE SETUP #############
+#module load xxx
+#export PATH="$PATH:/mnt/home/guid/software/xxx/bin"
+
+############# MY CODE #############
+echo "Hello from $SLURM_JOB_NODELIST"
